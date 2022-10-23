9ece661e413c0037f21951ac0c63336535a50ee9
seungbok.lee <seungbok.lee@aibizon.com>
2022-10-24 01:45:33 +0900

    misc format 추가, movie Document 추가

---
+++ src/main/java/com/example/mongojpapractice/controller/mflix/MflixMovieController.java
@@ -0,0 +1,34 @@
+package com.example.mongojpapractice.controller.mflix;
+
+import com.example.mongojpalogic.mflix.document.Movies;
+import com.example.mongojpalogic.mflix.service.MflixMovieService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/mflix")
+@RestController
+public class MflixMovieController {
+
+    private final MflixMovieService mflixMovieService;
+
+    @GetMapping(value = "/movies")
+    public ResponseEntity<List<Movies>> getMflixMovies() {
+        List<Movies> mflixMovies = mflixMovieService.getMflixMovies();
+        return ResponseEntity.status(HttpStatus.OK).body(mflixMovies);
+    }
+
+    @GetMapping(value = "/movie/{id}")
+    public ResponseEntity<Movies> getMflixMovie(@PathVariable ObjectId id) {
+        Movies mflixMovie = mflixMovieService.getMflixMovie(id);
+        return ResponseEntity.status(HttpStatus.OK).body(mflixMovie);
+    }
+
+}
--- src/main/java/com/example/mongojpapractice/controller/mflix/MflixUserController.java
+++ src/main/java/com/example/mongojpapractice/controller/mflix/MflixUserController.java
@@ -1,10 +1,12 @@
 package com.example.mongojpapractice.controller.mflix;
 
 import com.example.mongojpalogic.dto.TestDto;
+import com.example.mongojpalogic.mflix.dto.CustomMflixUserRes;
 import com.example.mongojpalogic.mflix.dto.MflixUserRes;
-import com.example.mongojpalogic.mflix.service.MflixService;
+import com.example.mongojpalogic.mflix.service.MflixUserService;
 import com.example.mongojpapractice.config.constants.StatusCodes;
 import com.example.mongojpapractice.res.ApiResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,29 +14,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/mflix")
 @RestController
-public class MflixController {
+public class MflixUserController {
+    private final MflixUserService mflixUserService;
 
-    private final MflixService mflixService;
-
-    @GetMapping(value = "/sample-mflix/users")
+    @GetMapping(value = "/users")
     public ResponseEntity<List<MflixUserRes>> getMflixUsers() {
-        return ResponseEntity.ok().body(mflixService.getMflixUsers());
+        return ResponseEntity.ok().body(mflixUserService.getMflixUsers());
     }
 
-    @GetMapping(value = "agg/sample-mflix/users")
+    @GetMapping(value = "/v2/user")
+    public ResponseEntity<CustomMflixUserRes> getMflixUser() {
+        List<MflixUserRes> mflixUsers = mflixUserService.getMflixUsers();
+        return null;
+    }
+
+    @GetMapping(value = "agg/users")
     public ResponseEntity<Object> getMflixUsersAggregation(@RequestParam int age) {
-        Object mflixUsersWithAge = mflixService.getMflixUsersWithAge(age);
+        Object mflixUsersWithAge = mflixUserService.getMflixUsersWithAge(age);
         ApiResponse apiResponse = new ApiResponse(StatusCodes.S001, mflixUsersWithAge);
         return new ResponseEntity<>(apiResponse, StatusCodes.valueOf(apiResponse.getCode()).status);
     }
 
     @GetMapping(value = "/multi-module-test")
     public ResponseEntity<Object> getRectangle(@RequestParam int width, @RequestParam int height) {
+        TestDto testDto = new TestDto();
+        testDto.setWidth(width);
+        testDto.setHeight(height);
+        ApiResponse apiResponse = new ApiResponse(StatusCodes.S001, testDto);
+        return new ResponseEntity<>(apiResponse, StatusCodes.valueOf(apiResponse.getCode()).status);
+    }
+
+    @GetMapping(value = "/test")
+    public ResponseEntity<Object> justTest(
+        @RequestParam int width,
+        @RequestParam int height,
+        @RequestParam float radius
+    ) {
         TestDto testDto = new TestDto();
         testDto.setWidth(width);
         testDto.setHeight(height);
